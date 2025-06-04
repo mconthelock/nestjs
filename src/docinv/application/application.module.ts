2bf707328e725be186027cd19e058dc652d26a59
Onthelock <s.chalormsak@gmail.com>
2025-06-04 17:27:21 +0700

    check application

--- src/docinv/application/application.module.ts
+++ src/docinv/application/application.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApplicationService } from './application.service';
 import { ApplicationController } from './application.controller';
+import { Application } from './entities/application.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Application], 'amecConnection')],
   controllers: [ApplicationController],
   providers: [ApplicationService],
+  exports: [ApplicationService],
 })
 export class ApplicationModule {}

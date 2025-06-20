3264a6ec1440b394cc7c8730d633e8c707874aea
Onthelock <s.chalormsak@gmail.com>
2025-06-20 08:37:33 +0700

    check image not found

--- src/amec/users/users.controller.ts
+++ src/amec/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, NotFoundException } from '@nestjs/common';
 import { UsersService } from './users.service';
 // import { CreateUserDto } from './dto/create-user.dto';
 // import { UpdateUserDto } from './dto/update-user.dto';
@@ -15,6 +15,11 @@ export class UsersController {
   @Get('/image/:id')
   async findImage(@Param('id') id: string) {
     const response = await fetch(`http://webflow/images/emp/${id}.jpg`);
+
+    if (response.status !== 200) {
+      throw new NotFoundException('Data not found');
+    }
+
     const arrayBuffer = await response.arrayBuffer();
     const buffer = Buffer.from(arrayBuffer);
     const imageUrl = `data:image/jpeg;base64,${buffer.toString('base64')}`;

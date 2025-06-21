41ca5b8bf5cb045122323a5890030ad52dc5da5a
Onthelock <s.chalormsak@gmail.com>
2025-06-21 10:48:36 +0700

    update gateway

--- src/pis/pis.gateway.ts
+++ src/pis/pis.gateway.ts
@@ -6,7 +6,7 @@ import {
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway({ namespace: '/api/pis/' })
+@WebSocketGateway({ namespace: '/api/pis' })
 export class PisGateway {
   @WebSocketServer()
   server: Server;

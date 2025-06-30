82c78200cade59ba5d528f4754703687bc0e9e7f
Onthelock <s.chalormsak@gmail.com>
2025-06-30 08:42:32 +0700

    delete sp inquiry

--- src/spprogram/inquiries/dto/update-inquiry.input.ts
+++
@@ -1,8 +0,0 @@
-import { CreateInquiryInput } from './create-inquiry.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
-
-@InputType()
-export class UpdateInquiryInput extends PartialType(CreateInquiryInput) {
-  @Field(() => Int)
-  id: number;
-}
--- src/spprogram/inquiries/entities/inquiry.entity.ts
+++
@@ -1,7 +0,0 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
-
-@ObjectType()
-export class Inquiry {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
-}

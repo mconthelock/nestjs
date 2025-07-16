1498b902bb2da39608ca64f80d03e6463cc00477
onthelock <s.chalormsak@gmail.com>
2025-07-16 23:49:18 +0700

    fix: skip main branch

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,19 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import amecConfig from './databases/amec.config';
 
-import { AuthModule } from './auth/auth.module';
-import { AmecModule } from './amec/amec.module';
-import { AmecMfgModule } from './amecmfg/amecmfg.module';
-import { DocinvModule } from './docinv/docinv.module';
-import { gpreportModule } from './gpreport/gpreport.module';
-import { AS400Module } from './as400/as400.module';
-import { WebformModule } from './webform/webform.module';
-import { HeaderModule } from './idtag/header/header.module';
+// import { AuthModule } from './auth/auth.module';
+// import { AmecModule } from './amec/amec.module';
+// import { AmecMfgModule } from './amecmfg/amecmfg.module';
+// import { DocinvModule } from './docinv/docinv.module';
+// import { gpreportModule } from './gpreport/gpreport.module';
+// import { AS400Module } from './as400/as400.module';
+// import { WebformModule } from './webform/webform.module';
+// import { HeaderModule } from './idtag/header/header.module';
 import { SpModule } from './spprogram/sp.module';
 
-import { JobOrderModule } from './joborder/joborder.module';
-import { PisModule } from './pis/pis.module';
-import { ESCSModule } from './escs/escs.module';
+// import { JobOrderModule } from './joborder/joborder.module';
+// import { PisModule } from './pis/pis.module';
+// import { ESCSModule } from './escs/escs.module';
 
 @Module({
   imports: [
@@ -24,20 +24,20 @@ import { ESCSModule } from './escs/escs.module';
     }),
     TypeOrmModule.forRootAsync(amecConfig),
     //BB8 💣
-    AuthModule,
-    AmecModule,
-    AmecMfgModule,
-    DocinvModule,
-    gpreportModule,
-    AS400Module,
-    WebformModule,
-    HeaderModule,
+    // AuthModule,
+    // AmecModule,
+    // AmecMfgModule,
+    // DocinvModule,
+    // gpreportModule,
+    // AS400Module,
+    // WebformModule,
+    // HeaderModule,
     SpModule,
 
     //JB 🤴
-    JobOrderModule,
-    PisModule,
-    ESCSModule,
+    // JobOrderModule,
+    // PisModule,
+    // ESCSModule,
   ],
 })
 export class AppModule {}

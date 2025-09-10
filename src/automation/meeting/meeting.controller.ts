d2965d1461a6e6d1e13c11af63fb26c12e4bc1bb
Chalormsak S. <s.chalormsak@gmail.com>
2025-09-10 12:14:21 +0700

    feat: add automate

--- src/automation/meeting/meeting.controller.ts
+++ src/automation/meeting/meeting.controller.ts
@@ -1,4 +1,10 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 import { MeetingService } from './meeting.service';
 
 @Controller('automate/meeting')
@@ -7,6 +13,77 @@ export class MeetingController {
 
   @Post('create')
   async create(@Body() body: any[]) {
-    return this.meeting.setMeeting(body);
+    try {
+      const { browser, page } = await this.meeting.initSession(body);
+
+      const sbutton = page.locator('button[aria-label^="start date"]');
+      await sbutton.click();
+      const scalendar = await sbutton.getAttribute('aria-owns');
+      const stime = page.locator('input[aria-label="start time"]');
+      await this.meeting.selectDate(
+        page,
+        scalendar,
+        stime,
+        body['startdate'],
+        body['starttime'],
+      );
+
+      const ebutton = page.locator('button[aria-label^="end date"]');
+      await ebutton.click();
+      const ecalendar = await ebutton.getAttribute('aria-owns');
+      const etime = page.locator('input[aria-label="end time"]');
+      await this.meeting.selectDate(
+        page,
+        ecalendar,
+        etime,
+        body['enddate'],
+        body['endtime'],
+      );
+      const rooms = await this.meeting.roomlist(page);
+      const roomselect = await this.meeting.selectRoom(page, body['room']);
+      await this.meeting.participants(page, body['attendees']);
+      await this.meeting.messages(page, body);
+      await page.getByRole('button', { name: 'Send' }).click();
+      await page.waitForLoadState('networkidle');
+      await this.meeting.logout(page);
+    } catch (err) {
+      throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
+    }
+  }
+
+  @Post('room')
+  async findroom(@Body() body: any[]) {
+    try {
+      const { browser, page } = await this.meeting.initSession(body);
+      //Start Date
+      const sbutton = page.locator('button[aria-label^="start date"]');
+      await sbutton.click();
+      const scalendar = await sbutton.getAttribute('aria-owns');
+      const stime = page.locator('input[aria-label="start time"]');
+      await this.meeting.selectDate(
+        page,
+        scalendar,
+        stime,
+        '2025-09-20',
+        '12:00 AM',
+      );
+
+      //End Date
+      const ebutton = page.locator('button[aria-label^="end date"]');
+      await ebutton.click();
+      const ecalendar = await ebutton.getAttribute('aria-owns');
+      const etime = page.locator('input[aria-label="end time"]');
+      await this.meeting.selectDate(
+        page,
+        ecalendar,
+        etime,
+        '2025-09-20',
+        '03:00 PM',
+      );
+      const rooms = this.meeting.roomlist(page);
+      return rooms;
+    } catch (err) {
+      return false;
+    }
   }
 }

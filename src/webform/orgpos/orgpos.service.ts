099d0fef3e8b8a24269a9493c130a0eadff76de2
sutthipongjab <sutthipongjab@gmail.com>
2025-09-25 10:31:46 +0700

    feat(orgpos): add Orgpos entity relation to User, update Orgpos service and controller for new functionalities

--- src/webform/orgpos/orgpos.service.ts
+++ src/webform/orgpos/orgpos.service.ts
@@ -16,6 +16,8 @@ export class OrgposService {
   ) {}
 
   async getOrgPos(dto: SearchOrgpoDto, queryRunner?: QueryRunner) {
+    console.log(dto);
+    
     const repo = queryRunner ? queryRunner.manager.getRepository(Orgpos) : this.orgpos;
     return repo.find({
       where: dto,

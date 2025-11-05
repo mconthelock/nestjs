1b92ebfb9bd0f88a2c2164de4e45b21a14d545ec
sutthipongjab <sutthipongjab@gmail.com>
2025-11-05 11:08:18 +0700

    feat: enhance date formatting in FlowService and update DTOs for better data handling

--- src/ids/matrix/matrix-item-master/dto/update-matrix-item-master.dto.ts
+++ src/ids/matrix/matrix-item-master/dto/update-matrix-item-master.dto.ts
@@ -1,4 +1,11 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateMatrixItemMasterDto } from './create-matrix-item-master.dto';
+import { IsNotEmpty, IsNumber } from 'class-validator';
+import { Type } from 'class-transformer';
 
-export class UpdateMatrixItemMasterDto extends PartialType(CreateMatrixItemMasterDto) {}
+export class UpdateMatrixItemMasterDto extends PartialType(CreateMatrixItemMasterDto) {
+    @IsNotEmpty()
+    @Type(() => Number)
+    @IsNumber()
+    ID: number;
+}

4cf4f527bf83b2d2068b12e58dc626c922eb0772
onthelock <s.chalormsak@gmail.com>
2026-02-07 15:27:31 +0700

    feat: integrate AppsgroupsModule and enhance ApplicationService with group creation logic

--- src/docinv/application/application.module.ts
+++ src/docinv/application/application.module.ts
@@ -3,9 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApplicationService } from './application.service';
 import { ApplicationController } from './application.controller';
 import { Application } from './entities/application.entity';
+import { AppsgroupsModule } from '../appsgroups/appsgroups.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Application], 'docinvConnection')],
+  imports: [
+    AppsgroupsModule,
+    TypeOrmModule.forFeature([Application], 'docinvConnection'),
+  ],
   controllers: [ApplicationController],
   providers: [ApplicationService],
   exports: [ApplicationService],

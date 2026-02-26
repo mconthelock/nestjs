36dafaff9bd25c19f138bae8c7e0a81079916318
Supamid <supamid@MitsubishiElevatorAsia.co.th>
2026-02-26 19:13:54 +0700

    Refactor code structure for improved readability and maintainability

--- src/gpreport/overtime/overtime.service.ts
+++ src/gpreport/overtime/overtime.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { CreateOvertimeDto } from './dto/create-overtime.dto';
 import { UpdateOvertimeDto } from './dto/update-overtime.dto';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { InjectRepository, InjectDataSource } from '@nestjs/typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { SearchOvertimeDto } from './dto/search-overtime.dto';
 import { Overtime } from 'src/common/Entities/gpreport/table/overtime.entity';
 
 @Injectable()
 export class OvertimeService {
   constructor(
-  @InjectRepository(Overtime, 'gpreportConnection')
-  private readonly otRepo: Repository<Overtime>,
-) {}
+    @InjectRepository(Overtime, 'gpreportConnection')
+    private readonly otRepo: Repository<Overtime>,
+
+    @InjectDataSource('gpreportConnection')
+      private readonly dataSource: DataSource,
+  ) {}
 
  
   create(createOvertimeDto: CreateOvertimeDto) {
@@ -22,7 +25,6 @@ export class OvertimeService {
   async findAll(q: SearchOvertimeDto) {
     return await this.otRepo.find({ where: q });
   }
-
 
 
   findOne(id: number) {
@@ -36,4 +38,19 @@ export class OvertimeService {
   remove(id: number) {
     return `This action removes a #${id} overtime`;
   }
+
+  async getOtByWorkdate(workdate: string) {
+    return await this.dataSource
+      .createQueryBuilder()
+      .select(['A.EMPNO AS EMPNO',`TO_CHAR(A.WORKDATE,'DD-MM-YYYY') AS WORKDATE`,'A.TIMEIN AS TIMEIN', 'A.TIMEOUT AS TIMEOUT', 'A.WKTYPENO AS WKTYPENO'
+        ,'B.CST AS CST','C.SNAME AS SNAME','C.SSEC AS SSEC','C.SDEPT AS SDEPT','C.SDIV AS SDIV',])
+      .from('OTFORM', 'A')
+      .innerJoin('FORM', 'B',`A.NFRMNO = B.NFRMNO AND A.VORGNO = B.VORGNO AND A.CYEAR = B.CYEAR AND A.CYEAR2 = B.CYEAR2`,)
+      .leftJoin('AMECUSERALL', 'C', 'A.EMPNO = C.SEMPNO')
+      .where(`(A.WORKDATE) = TO_DATE(:workdate,'DD-MM-YYYY')`, { workdate })
+      .andWhere('B.CST IN (1,2)')
+      .getRawMany();
+  }
+
+
 }

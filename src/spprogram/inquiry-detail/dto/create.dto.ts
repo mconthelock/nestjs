4f6ab5b4274f679f36296a2983af2c98552aca39
Chalormsak S. <s.chalormsak@gmail.com>
2026-03-04 09:20:55 +0700

    refactor: clean up code formatting and improve readability in main.ts, create.dto.ts, and inquiry-detail.entity.ts

--- src/spprogram/inquiry-detail/dto/create.dto.ts
+++ src/spprogram/inquiry-detail/dto/create.dto.ts
@@ -2,182 +2,181 @@ import { Type } from 'class-transformer';
 import { IsString, IsOptional, IsDate, IsNumber } from 'class-validator';
 
 export class createDetailDto {
-  @IsNumber()
-  @IsOptional()
-  @Type(() => Number)
-  INQD_ID: number;
+    @IsNumber()
+    @IsOptional()
+    @Type(() => Number)
+    INQD_ID: number;
 
-  @IsNumber()
-  @Type(() => Number)
-  INQG_GROUP: number;
+    @IsNumber()
+    @Type(() => Number)
+    INQG_GROUP: number;
 
-  @IsNumber()
-  @Type(() => Number)
-  INQD_SEQ: number;
+    @IsNumber()
+    @Type(() => Number)
+    INQD_SEQ: number;
 
-  @IsNumber()
-  @Type(() => Number)
-  INQD_RUNNO: number;
+    @IsNumber()
+    @Type(() => Number)
+    INQD_RUNNO: number;
 
-  @IsString()
-  @IsOptional()
-  INQD_MFGORDER: string;
+    @IsString()
+    @IsOptional()
+    INQD_MFGORDER: string;
 
-  @IsString()
-  @IsOptional()
-  INQD_ITEM: string;
+    @IsString()
+    @IsOptional()
+    INQD_ITEM: string;
 
-  @IsString()
-  @IsOptional()
-  INQD_CAR: string;
+    @IsString()
+    @IsOptional()
+    INQD_CAR: string;
 
-  @IsString()
-  @IsOptional()
-  INQD_PARTNAME: string;
+    @IsString()
+    @IsOptional()
+    INQD_PARTNAME: string;
 
-  @IsString()
-  @IsOptional()
-  INQD_DRAWING: string;
+    @IsString()
+    @IsOptional()
+    INQD_DRAWING: string;
 
-  @IsString()
-  @IsOptional()
-  INQD_VARIABLE: string;
+    @IsString()
+    @IsOptional()
+    INQD_VARIABLE: string;
 
-  @IsNumber()
-  @Type(() => Number)
-  INQD_QTY: number;
+    @IsNumber()
+    @Type(() => Number)
+    INQD_QTY: number;
 
-  @IsString()
-  @IsOptional()
-  INQD_UM: string;
+    @IsString()
+    @IsOptional()
+    INQD_UM: string;
 
-  @IsString()
-  @IsOptional()
-  INQD_SUPPLIER: string;
+    @IsString()
+    @IsOptional()
+    INQD_SUPPLIER: string;
 
-  @IsNumber()
-  @IsOptional()
-  @Type(() => Number)
-  INQD_SENDPART: number;
+    @IsString()
+    @IsOptional()
+    INQD_SENDPART: string;
 
-  @IsNumber()
-  @IsOptional()
-  @Type(() => Number)
-  INQD_UNREPLY: number;
+    @IsNumber()
+    @IsOptional()
+    @Type(() => Number)
+    INQD_UNREPLY: number;
 
-  @IsNumber()
-  @IsOptional()
-  @Type(() => Number)
-  INQD_FC_COST: number;
+    @IsNumber()
+    @IsOptional()
+    @Type(() => Number)
+    INQD_FC_COST: number;
 
-  @IsNumber()
-  @IsOptional()
-  @Type(() => Number)
-  INQD_TC_COST: number;
+    @IsNumber()
+    @IsOptional()
+    @Type(() => Number)
+    INQD_TC_COST: number;
 
-  @IsNumber()
-  @IsOptional()
-  @Type(() => Number)
-  INQD_UNIT_PRICE: number;
+    @IsNumber()
+    @IsOptional()
+    @Type(() => Number)
+    INQD_UNIT_PRICE: number;
 
-  @IsNumber()
-  @IsOptional()
-  @Type(() => Number)
-  INQD_FC_BASE: number;
+    @IsNumber()
+    @IsOptional()
+    @Type(() => Number)
+    INQD_FC_BASE: number;
 
-  @IsNumber()
-  @IsOptional()
-  @Type(() => Number)
-  INQD_TC_BASE: number;
+    @IsNumber()
+    @IsOptional()
+    @Type(() => Number)
+    INQD_TC_BASE: number;
 
-  @IsString()
-  @IsOptional()
-  INQD_MAR_REMARK: string;
+    @IsString()
+    @IsOptional()
+    INQD_MAR_REMARK: string;
 
-  @IsString()
-  @IsOptional()
-  INQD_DES_REMARK: string;
+    @IsString()
+    @IsOptional()
+    INQD_DES_REMARK: string;
 
-  @IsString()
-  @IsOptional()
-  INQD_FIN_REMARK: string;
+    @IsString()
+    @IsOptional()
+    INQD_FIN_REMARK: string;
 
-  @IsNumber()
-  @IsOptional()
-  @Type(() => Number)
-  INQD_LATEST: number;
+    @IsNumber()
+    @IsOptional()
+    @Type(() => Number)
+    INQD_LATEST: number;
 
-  @IsString()
-  @IsOptional()
-  INQD_OWNER: string;
+    @IsString()
+    @IsOptional()
+    INQD_OWNER: string;
 
-  @IsDate()
-  @IsOptional()
-  @Type(() => Date)
-  CREATE_AT: Date;
+    @IsDate()
+    @IsOptional()
+    @Type(() => Date)
+    CREATE_AT: Date;
 
-  @IsString()
-  @IsOptional()
-  CREATE_BY: string;
+    @IsString()
+    @IsOptional()
+    CREATE_BY: string;
 
-  @IsDate()
-  @IsOptional()
-  @Type(() => Date)
-  UPDATE_AT: Date;
+    @IsDate()
+    @IsOptional()
+    @Type(() => Date)
+    UPDATE_AT: Date;
 
-  @IsString()
-  @IsOptional()
-  UPDATE_BY: string;
+    @IsString()
+    @IsOptional()
+    UPDATE_BY: string;
 
-  @IsString()
-  @IsOptional()
-  INQD_COMPARE: string;
+    @IsString()
+    @IsOptional()
+    INQD_COMPARE: string;
 
-  @IsString()
-  @IsOptional()
-  INQD_COMPARE_DATE: string;
+    @IsString()
+    @IsOptional()
+    INQD_COMPARE_DATE: string;
 
-  @IsString()
-  @IsOptional()
-  INQD_OWNER_GROUP: string;
+    @IsString()
+    @IsOptional()
+    INQD_OWNER_GROUP: string;
 
-  @IsNumber()
-  @IsOptional()
-  @Type(() => Number)
-  ITEMID: number;
+    @IsNumber()
+    @IsOptional()
+    @Type(() => Number)
+    ITEMID: number;
 
-  @IsNumber()
-  @IsOptional()
-  @Type(() => Number)
-  INQID: number;
+    @IsNumber()
+    @IsOptional()
+    @Type(() => Number)
+    INQID: number;
 
-  @IsString()
-  @IsOptional()
-  TEST_FLAG: string;
+    @IsString()
+    @IsOptional()
+    TEST_FLAG: string;
 
-  @IsString()
-  @IsOptional()
-  TEST_MESSAGE: string;
+    @IsString()
+    @IsOptional()
+    TEST_MESSAGE: string;
 
-  @IsString()
-  @IsOptional()
-  AUTO_ADD: string;
+    @IsString()
+    @IsOptional()
+    AUTO_ADD: string;
 
-  @IsNumber()
-  @IsOptional()
-  @Type(() => Number)
-  INQD_PREV: number;
+    @IsNumber()
+    @IsOptional()
+    @Type(() => Number)
+    INQD_PREV: number;
 
-  @IsString()
-  @IsOptional()
-  UPDATE_CODE: string;
+    @IsString()
+    @IsOptional()
+    UPDATE_CODE: string;
 
-  @IsNumber()
-  @IsOptional()
-  @Type(() => Number)
-  INQD_EXRATE: number;
+    @IsNumber()
+    @IsOptional()
+    @Type(() => Number)
+    INQD_EXRATE: number;
 
-  @IsString()
-  @IsOptional()
-  INQD_DE: string;
+    @IsString()
+    @IsOptional()
+    INQD_DE: string;
 }
--- src/spprogram/inquiry-detail/entities/inquiry-detail.entity.ts
+++ src/spprogram/inquiry-detail/entities/inquiry-detail.entity.ts
@@ -1,12 +1,12 @@
 import {
-  Entity,
-  PrimaryColumn,
-  Column,
-  OneToOne,
-  JoinColumn,
-  OneToMany,
-  ManyToOne,
-  PrimaryGeneratedColumn,
+    Entity,
+    PrimaryColumn,
+    Column,
+    OneToOne,
+    JoinColumn,
+    OneToMany,
+    ManyToOne,
+    PrimaryGeneratedColumn,
 } from 'typeorm';
 import { Inquiry } from '../../inquiry/entities/inquiry.entity';
 import { InquiryGroup } from '../../inquiry-group/entities/inquiry-group.entity';
@@ -15,138 +15,138 @@ import { InquiryLog } from '../../inquiry-log/entities/inquiry-log.entity';
 
 @Entity('SP_INQUIRY_DETAIL')
 export class InquiryDetail {
-  @PrimaryGeneratedColumn()
-  INQD_ID: number;
+    @PrimaryGeneratedColumn()
+    INQD_ID: number;
 
-  @Column()
-  INQG_GROUP: number;
+    @Column()
+    INQG_GROUP: number;
 
-  @Column('decimal', { precision: 10, scale: 2 })
-  INQD_SEQ: number;
+    @Column('decimal', { precision: 10, scale: 2 })
+    INQD_SEQ: number;
 
-  @Column()
-  INQD_RUNNO: number;
+    @Column()
+    INQD_RUNNO: number;
 
-  @Column()
-  INQD_MFGORDER: string;
+    @Column()
+    INQD_MFGORDER: string;
 
-  @Column()
-  INQD_ITEM: string;
+    @Column()
+    INQD_ITEM: string;
 
-  @Column()
-  INQD_CAR: string;
+    @Column()
+    INQD_CAR: string;
 
-  @Column()
-  INQD_PARTNAME: string;
+    @Column()
+    INQD_PARTNAME: string;
 
-  @Column()
-  INQD_DRAWING: string;
+    @Column()
+    INQD_DRAWING: string;
 
-  @Column()
-  INQD_VARIABLE: string;
+    @Column()
+    INQD_VARIABLE: string;
 
-  @Column()
-  INQD_QTY: number;
+    @Column()
+    INQD_QTY: number;
 
-  @Column()
-  INQD_UM: string;
+    @Column()
+    INQD_UM: string;
 
-  @Column()
-  INQD_SUPPLIER: string;
+    @Column()
+    INQD_SUPPLIER: string;
 
-  @Column()
-  INQD_SENDPART: number;
+    @Column()
+    INQD_SENDPART: string;
 
-  @Column()
-  INQD_UNREPLY: number;
+    @Column()
+    INQD_UNREPLY: number;
 
-  @Column('decimal', { precision: 10, scale: 2 })
-  INQD_FC_COST: number;
+    @Column('decimal', { precision: 15, scale: 4 })
+    INQD_FC_COST: number;
 
-  @Column('decimal', { precision: 10, scale: 2 })
-  INQD_TC_COST: number;
+    @Column('decimal', { precision: 15, scale: 4 })
+    INQD_TC_COST: number;
 
-  @Column('decimal', { precision: 10, scale: 2 })
-  INQD_UNIT_PRICE: number;
+    @Column('decimal', { precision: 15, scale: 4 })
+    INQD_UNIT_PRICE: number;
 
-  @Column('decimal', { precision: 10, scale: 2 })
-  INQD_FC_BASE: number;
+    @Column('decimal', { precision: 15, scale: 4 })
+    INQD_FC_BASE: number;
 
-  @Column('decimal', { precision: 10, scale: 2 })
-  INQD_TC_BASE: number;
+    @Column('decimal', { precision: 15, scale: 4 })
+    INQD_TC_BASE: number;
 
-  @Column()
-  INQD_MAR_REMARK: string;
+    @Column()
+    INQD_MAR_REMARK: string;
 
-  @Column()
-  INQD_DES_REMARK: string;
+    @Column()
+    INQD_DES_REMARK: string;
 
-  @Column()
-  INQD_FIN_REMARK: string;
+    @Column()
+    INQD_FIN_REMARK: string;
 
-  @Column()
-  INQD_LATEST: number;
+    @Column()
+    INQD_LATEST: number;
 
-  @Column()
-  INQD_OWNER: string;
+    @Column()
+    INQD_OWNER: string;
 
-  @Column()
-  CREATE_AT: Date;
+    @Column()
+    CREATE_AT: Date;
 
-  @Column()
-  CREATE_BY: string;
+    @Column()
+    CREATE_BY: string;
 
-  @Column()
-  UPDATE_AT: Date;
+    @Column()
+    UPDATE_AT: Date;
 
-  @Column()
-  UPDATE_BY: string;
+    @Column()
+    UPDATE_BY: string;
 
-  @Column()
-  INQD_COMPARE: string;
+    @Column()
+    INQD_COMPARE: string;
 
-  @Column()
-  INQD_COMPARE_DATE: string;
+    @Column()
+    INQD_COMPARE_DATE: string;
 
-  @Column()
-  INQD_OWNER_GROUP: string;
+    @Column()
+    INQD_OWNER_GROUP: string;
 
-  @Column()
-  ITEMID: number;
+    @Column()
+    ITEMID: number;
 
-  @Column()
-  INQID: number;
+    @Column()
+    INQID: number;
 
-  @Column()
-  TEST_FLAG: string;
+    @Column()
+    TEST_FLAG: string;
 
-  @Column()
-  TEST_MESSAGE: string;
+    @Column()
+    TEST_MESSAGE: string;
 
-  @Column()
-  AUTO_ADD: string;
+    @Column()
+    AUTO_ADD: string;
 
-  @Column()
-  INQD_PREV: number;
+    @Column()
+    INQD_PREV: number;
 
-  @Column()
-  UPDATE_CODE: string;
+    @Column()
+    UPDATE_CODE: string;
 
-  @Column()
-  INQD_EXRATE: number;
+    @Column()
+    INQD_EXRATE: number;
 
-  @Column()
-  INQD_DE: string;
+    @Column()
+    INQD_DE: string;
 
-  @ManyToOne(() => Inquiry, (inq) => inq.details)
-  @JoinColumn({ name: 'INQID', referencedColumnName: 'INQ_ID' })
-  inqs: Inquiry;
+    @ManyToOne(() => Inquiry, (inq) => inq.details)
+    @JoinColumn({ name: 'INQID', referencedColumnName: 'INQ_ID' })
+    inqs: Inquiry;
 
-  @ManyToOne(() => InquiryGroup, (group) => group.details)
-  @JoinColumn({ name: 'INQG_GROUP', referencedColumnName: 'INQG_ID' })
-  inqgroup: InquiryGroup;
+    @ManyToOne(() => InquiryGroup, (group) => group.details)
+    @JoinColumn({ name: 'INQG_GROUP', referencedColumnName: 'INQG_ID' })
+    inqgroup: InquiryGroup;
 
-  @OneToMany(() => InquiryLog, (logs) => logs.details)
-  @JoinColumn({ name: 'INQD_ID', referencedColumnName: 'INQD_ID' })
-  logs: InquiryLog;
+    @OneToMany(() => InquiryLog, (logs) => logs.details)
+    @JoinColumn({ name: 'INQD_ID', referencedColumnName: 'INQD_ID' })
+    logs: InquiryLog;
 }

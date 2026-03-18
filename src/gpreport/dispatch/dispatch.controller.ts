6bd1b237bc5a471ded2011d955a7892866f8f335
Supamid <supamid@MitsubishiElevatorAsia.co.th>
2026-03-18 11:45:55 +0700

    update #20260318 byG #3

--- src/gpreport/dispatch/dispatch.controller.ts
+++ src/gpreport/dispatch/dispatch.controller.ts
@@ -1,11 +1,9 @@
 import { Body, Controller, Post, Param } from '@nestjs/common';
 import { DispatchService } from './dispatch.service';
-import { SaveDispatchDto } from './dto/save-dispatch.dto';
 import { SaveOverwriteDto } from './dto/save-overwrite.dto';
 import { BuildDailyFirstDto } from './dto/build-daily-first.dto';
 import { DispatchKeyDto } from './dto/dispatch-key.dto';
 import { MoveStopDto } from './dto/move-stop.dto';
-import { DeleteLineDto } from './dto/delete-line.dto';
 import { SaveAddPassengerDto } from './dto/save-add-passenger.dto';
 import { IsNotEmpty, IsString } from 'class-validator';
 import { DailyDispatchReportDto } from './dto/dispatch-report.dto';

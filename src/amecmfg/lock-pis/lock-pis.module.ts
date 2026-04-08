7225a667e9f93e39ad7c3bed7486de974858e0d1
sutthipongjab <sutthipongjab@gmail.com>
2026-04-08 10:59:59 +0700

    feat: refactor LockPisModule to use RedisModule and clean up provider imports

--- src/amecmfg/lock-pis/lock-pis.module.ts
+++ src/amecmfg/lock-pis/lock-pis.module.ts
@@ -6,11 +6,25 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { LockPis } from './entities/lock-pi.entity';
 import { LockPisController } from './lock-pis.controller';
 import { LockPisDBGateway } from './lock-pisDB.gatewey';
-import { redisProvider, redisSubProvider } from 'src/common/redis/redis.provider';
+import { RedisModule } from 'src/common/redis/redis.module';
+// import {
+//     redisProvider,
+//     redisSubProvider,
+// } from 'src/common/redis/redis.provider';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([LockPis], 'webformConnection'), UsersModule],
-  controllers: [LockPisController],
-  providers: [LockPisGateway, LockPisDBGateway, LockPisService, redisProvider, redisSubProvider],
+    imports: [
+        TypeOrmModule.forFeature([LockPis], 'webformConnection'),
+        UsersModule,
+        RedisModule,
+    ],
+    controllers: [LockPisController],
+    providers: [
+        LockPisGateway,
+        LockPisDBGateway,
+        LockPisService,
+        // redisProvider,
+        // redisSubProvider,
+    ],
 })
 export class LockPisModule {}

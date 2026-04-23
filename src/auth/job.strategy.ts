001be429efcf7cd2c7bcd2e4479b0a6fb424f71b
Chalormsak S. <s.chalormsak@gmail.com>
2026-04-23 08:12:01 +0700

    fix: bypass ApiKeyGuard

--- src/auth/job.strategy.ts
+++ src/auth/job.strategy.ts
@@ -1,29 +1,32 @@
 import {
-  Injectable,
-  CanActivate,
-  ExecutionContext,
-  UnauthorizedException,
+    Injectable,
+    CanActivate,
+    ExecutionContext,
+    UnauthorizedException,
 } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class ApiKeyGuard implements CanActivate {
-  constructor(private readonly configService: ConfigService) {}
+    constructor(private readonly configService: ConfigService) {}
 
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    const request = context.switchToHttp().getRequest();
-    const keyFromHeader = request.headers['x-api-key'];
-    const validKey = this.configService.get<string>('JOB_SCHEDULER_API_KEY');
-    if (!validKey) {
-      throw new Error('JOB_SCHEDULER_API_KEY is not set in environment');
-    }
+    canActivate(
+        context: ExecutionContext,
+    ): boolean | Promise<boolean> | Observable<boolean> {
+        const request = context.switchToHttp().getRequest();
+        const keyFromHeader = request.headers['x-api-key'];
+        const validKey = this.configService.get<string>(
+            'JOB_SCHEDULER_API_KEY',
+        );
+        if (!validKey) {
+            throw new Error('JOB_SCHEDULER_API_KEY is not set in environment');
+        }
 
-    if (keyFromHeader === validKey) {
-      return true;
+        if (keyFromHeader === validKey) {
+            return true;
+        }
+        return true;
+        throw new UnauthorizedException('Invalid API Key');
     }
-    throw new UnauthorizedException('Invalid API Key');
-  }
 }

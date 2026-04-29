ecf4b6e9705afe91a310fdec50d9cfebf54dded1
Supakitt <supakitt@mitsubishielevatorasia.co.th>
2026-04-29 11:30:35 +0700

    fin-ds get stamp

--- src/webform/finform/fin-ds/fin-ds.module.ts
+++ src/webform/finform/fin-ds/fin-ds.module.ts
@@ -11,15 +11,17 @@ import {DSDUTYSTAMP } from  'src/common/Entities/webform/table/FINDS_DUTY_STAMP.
 import {DSREQDETAIL } from  'src/common/Entities/webform/table/FINDS_REQ_DETAIL.entity'
 import {DSSTOCK } from  'src/common/Entities/webform/table/FINDS_STOCK.entity'
 import {DSREQHEAD } from  'src/common/Entities/webform/table/FINDS_REQ_HEAD.entity'
+import { FinDsRepository } from './fin-ds.repository';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([DSDUTYSTAMP,DSREQDETAIL,DSSTOCK,DSREQHEAD],'webformConnection'),
+  imports:[
+      TypeOrmModule.forFeature([DSDUTYSTAMP], 'webformConnection'),
       FormModule,
       FlowModule,
   ],
 
   controllers: [FinDsController],
-  providers: [FinDsService],
+  providers: [FinDsService,FinDsRepository],
 
 })
 export class FinDsModule {}

e59f11b44b2fc76841b3a825d0fa2ee1fc91ddcd
chutipongp <chutiphongp@MitsubishiElevatorAsia.co.th>
2026-04-30 14:21:52 +0700

    getdata form to table

--- src/webform/gpform/gp-rb/gp-rb.repository.ts
+++ src/webform/gpform/gp-rb/gp-rb.repository.ts
@@ -1,17 +1,26 @@
-import { Injectable } from "@nestjs/common";
-import { InjectDataSource } from "@nestjs/typeorm";
-import { RB_PURPOSE } from "src/common/Entities/webform/table/GPRB_PURPOSE.entity";
-import { BaseRepository } from "src/common/repositories/base-repository";
-import { DataSource } from "typeorm";
+import { Injectable } from '@nestjs/common';
+import { InjectDataSource } from '@nestjs/typeorm';
+import { RB_PURPOSE } from 'src/common/Entities/webform/table/GPRB_PURPOSE.entity';
+import { BaseRepository } from 'src/common/repositories/base-repository';
+import { DataSource } from 'typeorm';
+import { CreateCusStampReqDto, CreateGpRbDto, CreateStampReqDto } from './dto/create-gp-rb.dto';
+import { RB_STAMP_REQ } from 'src/common/Entities/webform/table/GPRB_STAMP_REQ.entity';
+import { RB_CUS_STAMP_REQ } from 'src/common/Entities/webform/table/GPRB_CUS_STAMP_REQ.entity';
 
 @Injectable()
-export class GpRbRepository  extends BaseRepository{
-    constructor(
-        @InjectDataSource('webformConnection') ds:DataSource,
-    ){
-            super(ds);
-        }
-    findAll(){
-  return this.getRepository(RB_PURPOSE).find();
- }
+export class GpRbRepository extends BaseRepository {
+    constructor(@InjectDataSource('webformConnection') ds: DataSource) {
+        super(ds);
+    }
+    findAll() {
+        return this.getRepository(RB_PURPOSE).find();
+    }
+
+    async CreateStampReq(dto:CreateStampReqDto) {
+        return this.getRepository(RB_STAMP_REQ).save(dto);
+    }
+
+    async CreateCusStampReq(dto:CreateCusStampReqDto) {
+        return this.getRepository(RB_CUS_STAMP_REQ).save(dto);
+    }
 }
--- src/webform/gpform/gp-rb/gp-rb.service.ts
+++ src/webform/gpform/gp-rb/gp-rb.service.ts
@@ -26,13 +26,41 @@ export class GpRbService {
                     NFRMNO: formmst.NNO,
                     VORGNO: formmst.VORGNO,
                     CYEAR: formmst.CYEAR,
-                    REQBY: dto.reqCode,        
-                    INPUTBY: dto.inputBy
+                    REQBY: dto.REQBY,
+                    INPUTBY: dto.INPUTBY,
                 },
                 ip,
             );
+            const form = {
+                NFRMNO: createForm.data.NFRMNO,
+                VORGNO: createForm.data.VORGNO,
+                CYEAR: createForm.data.CYEAR,
+                CYEAR2: createForm.data.CYEAR2,
+                NRUNNO: createForm.data.NRUNNO,
+            };
             console.log('Form Master;', formmst);
-            return createForm;
+ 
+            /*สร้างเงื่อนไขที่เลือกระหว่างแบบที่ 1 หรือ 2*/
+            await this.repo.CreateStampReq({
+                ...form,
+                PURPOSE_ID: dto.PURPOSE_ID,
+                PURPOSE_OTHER: dto.PURPOSE_OTHER,
+                SPOSCODE: dto.SPOSCODE,
+                NAME_STAMP: dto.NAME_STAMP,
+                REMARK: dto.REMARK,
+            });
+            /*สร้างเงื่อนไขที่เลือกระหว่างแบบที่ 1 หรือ 2*/
+            await this.repo.CreateCusStampReq({
+                ...form,
+                CUST_SIZE: dto.CUST_SIZE,
+                QTY: dto.QTY,
+                REMARK: dto.REMARK,
+            });
+
+            return {
+                status: true,
+                message: 'GP-RB form created successfully',
+            }
         } catch (error) {
             throw new Error('Failed to ctrate GP-RB form');
         }

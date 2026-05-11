75722cef47493c8c6f998fc8c19a37d09f6a0baf
Supamid <supamid@MitsubishiElevatorAsia.co.th>
2026-05-11 15:07:07 +0700

    update for MFG-EDR

--- src/webform/mfgform/mfg-edr/dto/create-mfg-edr.dto.ts
+++ src/webform/mfgform/mfg-edr/dto/create-mfg-edr.dto.ts
@@ -1,1 +1,34 @@
-export class CreateMfgEdrDto {}
+export class CreateMfgEdrDto {
+  NFRMNO: number;
+  VORGNO: string;
+  CYEAR: string;
+  CYEAR2: string;
+  NRUNNO: number;
+
+  TID?: number | null;
+  SSECCODE?: string | null;
+  CID?: number | null;
+  REPAIR_BY?: string | null;
+  DAILY_MONTH?: string | null;
+  DAILY_RUNNO?: number | null;
+  REASON_CAUSE?: string | null;
+
+  list?: {
+    ORDERNO?: string | null;
+    DWGNO?: string | null;
+    ITEM?: string | null;
+    QTY?: number | null;
+    DETAIL?: string | null;
+    LV_EFFECT?: string | null;
+    EFFECT?: string | null;
+    LID?: number | null;
+    PID?: number | null;
+    LOT?: string | null;
+    SERIAL?: string | null;
+    PRDN_JUN?: string | null;
+  }[];
+
+  att?: {
+    FILENAME?: string | null;
+  }[];
+}

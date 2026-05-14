c01cfc4a192a28f5022935a27664f67b79f4314a
punnawichs <punnawichs@mitsubishielevatorasia.co.th>
2026-05-14 10:30:43 +0700

    prepare for update gprb

--- src/webform/gpform/gp-rb/gp-rb.controller.ts
+++ src/webform/gpform/gp-rb/gp-rb.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, Req, UseInterceptors
 import { GpRbService, ShowCusstampGpRbService, ShowstampGpRbService} from './gp-rb.service';
 import { CreateGpRbDto } from './dto/create-gp-rb.dto';
 import { UpdateGpRbDto } from './dto/update-gp-rb.dto';
-import { UseTransaction } from 'src/common/decorator/transaction.decorator';
+import { UseForceTransaction, UseTransaction } from 'src/common/decorator/transaction.decorator';
 import { getClientIP } from 'src/common/utils/ip.utils';
 import { Request } from "express";
 import { getFileUploadInterceptor } from 'src/common/helpers/file-upload.helper';
@@ -28,6 +28,8 @@ export class GpRbController {
     const ip = getClientIP(req)
     return this.gpRbServicee.create(dto, ip);
   }
+
+  
 }
 
 
@@ -46,6 +48,11 @@ export class ShowstampGpRbController {
   ){
     return this.gpRbServicee.findOne({NFRMNO: fno, VORGNO: orgno, CYEAR: cyear, CYEAR2: cyear2, NRUNNO: nrunno});
   }
+  
+  // @Patch()
+  // @UseTransaction('webformConnection') // ใส่เพื่อบอกว่าเปิด transaction กับการเชื่อมต่อ webformConnection
+  // @UseForceTransaction()
+  // update(@Body() dto: )
 
 }    
 
@@ -64,5 +71,6 @@ export class ShowCusStampGpRbController {
   ){
     return this.gpRbServicee.findOne({NFRMNO: fno, VORGNO: orgno, CYEAR: cyear, CYEAR2: cyear2, NRUNNO: nrunno});
    }
+  
 }
 

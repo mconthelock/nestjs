74732a08e21839f3a501668c3dd9b8dbd83420d5
ksatidpaisankul <ksatidpaisankul@gmail.com>
2026-05-27 09:20:14 +0700

    new vendor form

--- src/webform/purform/pur-nvf/purnvf_list/dto/create-purnvf_list.dto.ts
+++ src/webform/purform/pur-nvf/purnvf_list/dto/create-purnvf_list.dto.ts
@@ -1,1 +1,71 @@
-export class CreatePurnvfListDto {}
+import { PickType } from '@nestjs/swagger';
+import { Transform, Type } from 'class-transformer';
+import {
+    IsArray,
+    IsDate,
+    IsNotEmpty,
+    IsNumber,
+    IsOptional,
+    IsString,
+} from 'class-validator';
+import { FormDto }  from "src/webform/form/dto/form.dto";
+
+export class CreatePurnvfListDto extends PickType(FormDto,[
+  'NFRMNO',
+  'VORGNO',
+  'CYEAR',
+  'CYEAR2',
+  'NRUNNO',
+] as const) {    
+    @IsNotEmpty()
+    @IsNumber()
+    LID: number;
+
+    @IsString()
+    PURPOSE: string;
+
+    @IsString()
+    TYPEJOB: string;
+
+    @IsString()
+    SERVICE: string;   
+
+    @IsString()
+    REASON: string;   
+
+    @IsString()
+    VENDCODE: string;
+
+    @IsString()
+    VENDTYPE: string;
+
+    @IsString()
+    COMNAME: string;
+
+    @IsString()
+    CONTACT: string;
+
+    @IsString()
+    EMAIL: string;
+    
+    @IsString()
+    WEBSITE: string;
+
+    @IsString()
+    TELNO: string;
+
+    @IsString()
+    FAX: string;
+
+    @IsString()
+    BANKNAME: string;
+
+    @IsString()
+    BRANCH: string;
+
+    @IsString()
+    ACCNUMBER: string;
+
+    @IsString()
+    TERMCODE: string;
+}
--- src/webform/purform/pur-nvf/purnvf_list/purnvf_list.service.ts
+++ src/webform/purform/pur-nvf/purnvf_list/purnvf_list.service.ts
@@ -1,11 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePurnvfListDto } from './dto/create-purnvf_list.dto';
 import { UpdatePurnvfListDto } from './dto/update-purnvf_list.dto';
+import { FormDto } from 'src/webform/form/dto/form.dto';
+import { PurnvfListRepository } from './purnvf_list.repository';
 
 @Injectable()
 export class PurnvfListService {
-  create(createPurnvfListDto: CreatePurnvfListDto) {
-    return 'This action adds a new purnvfList';
+  constructor(private readonly repo: PurnvfListRepository) {}
+
+  async create(dto: CreatePurnvfListDto) {
+          try {
+            const res = await this.repo.create(dto);
+            if(!res){
+                throw new Error('Failed to insert PURVNFLIST');
+            }
+            return {
+                status: true,
+                message: 'Insert PURVNFLIST Successfully',
+            };
+        } catch (error) {
+            throw new Error('Insert PURVNFLIST Error: ' + error.message);
+        }
+
   }
 
   findAll() {

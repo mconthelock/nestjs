d25fc6b9049b5f9e1de2c701c565caf65bb56574
ksatidpaisankul <ksatidpaisankul@gmail.com>
2026-05-27 10:38:29 +0700

    vendor new form

---
+++ src/common/Entities/webform/table/PURVNF_ADDRESS.entity.ts
@@ -0,0 +1,62 @@
+import {
+    Column,
+    Entity,
+    JoinColumn,
+    ManyToOne,
+    OneToMany,
+    OneToOne,
+    PrimaryColumn,
+} from 'typeorm';
+import { PURNVF_FORM } from './PURVNF_FORM.entity';
+
+
+@Entity({ name: 'PURNVF_ADDRESS', schema: 'WEBFORM' })
+export class PURNVF_ADDRESS{
+    @PrimaryColumn()
+    NFRMNO: number;
+
+    @PrimaryColumn()
+    VORGNO: string;
+
+    @PrimaryColumn()
+    CYEAR: string;
+
+    @PrimaryColumn()
+    CYEAR2: string;
+
+    @PrimaryColumn()
+    NRUNNO: number;
+
+    @PrimaryColumn()
+    ADDRID: number;
+
+    @Column()
+    ADDRTYPE: string;
+
+    @Column()
+    ADDR: string;
+
+    @Column()
+    SUBDISTRICT: string;
+
+    @Column()
+    DISTRICT: string;
+
+    @Column()
+    PROVINCE: string;
+
+    @Column()
+    COUNTRY: string;
+
+    @Column()
+    POSTCODE: string;
+
+@ManyToOne(() => PURNVF_FORM, (nvf) => nvf.ADDRESSES)
+  @JoinColumn({ name: 'NFRMNO', referencedColumnName: 'NFRMNO' })
+  @JoinColumn({ name: 'VORGNO', referencedColumnName: 'VORGNO' })
+  @JoinColumn({ name: 'CYEAR', referencedColumnName: 'CYEAR' })
+  @JoinColumn({ name: 'CYEAR2', referencedColumnName: 'CYEAR2' })
+  @JoinColumn({ name: 'NRUNNO', referencedColumnName: 'NRUNNO' })
+  MASTER_NVFADDR : PURNVF_FORM;
+
+}

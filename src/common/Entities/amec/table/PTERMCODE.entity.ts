2a1940aafb52cb27f18d0585851867dd315c8a5a
ksatidpaisankul <ksatidpaisankul@gmail.com>
2026-05-27 08:16:13 +0700

    term of payments

--- src/common/Entities/amec/table/PTERMCODE.entity.ts
+++ src/common/Entities/amec/table/PTERMCODE.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryColumn } from 'typeorm';
+import { Column, Entity, PrimaryColumn , OneToMany } from 'typeorm';
+import { PURNVF_LIST } from '../../webform/table/PURVNF_LIST.entity'; 
 
 @Entity({ name: 'PTERMCODE', schema: 'AMEC' })
 export class PTERMCODE {
@@ -8,4 +9,11 @@ export class PTERMCODE {
     @Column()
     STERMDESC: string;
 
+    @OneToMany(() => PURNVF_LIST, (l) => l.TERM)
+    LISTS: PURNVF_LIST[];
+
 }
+
+
+
+

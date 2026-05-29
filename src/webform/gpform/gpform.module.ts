6b1e9e7908f9c5b58286c3446d02009aaa297ea8
worawutp <worawutp@mitsubishielevatorasia.co.th>
2026-05-29 09:53:49 +0700

    Add API GA Requisition

--- src/webform/gpform/gpform.module.ts
+++ src/webform/gpform/gpform.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { GpFileModule } from './gp-file/gp-file.module';
 import { GpRbModule, ShowCusStampGpRbModule, ShowstampGpRbModule } from './gp-rb/gp-rb.module';
+import { GpGarModule } from './gp-gar/gp-gar.module';
 
 @Module({
-    imports: [GpRbModule, GpFileModule,  ShowstampGpRbModule, ShowCusStampGpRbModule],
+    imports: [GpRbModule, GpFileModule, GpGarModule, ShowstampGpRbModule, ShowCusStampGpRbModule],
 })
 export class GpformModule {}
 

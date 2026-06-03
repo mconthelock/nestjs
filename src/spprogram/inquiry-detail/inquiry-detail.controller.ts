f86f2cf6290d294bae8ca6e33cbec0857ab18269
Chalormsak S. <s.chalormsak@gmail.com>
2026-06-03 13:54:18 +0700

    feat: implement update functionality and add updateDetailDto for inquiry details

--- src/spprogram/inquiry-detail/inquiry-detail.controller.ts
+++ src/spprogram/inquiry-detail/inquiry-detail.controller.ts
@@ -1,13 +1,19 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { InquiryDetailService } from './inquiry-detail.service';
 import { createDetailDto } from './dto/create.dto';
+import { updateDetailDto } from './dto/update.dto';
 
 @Controller('sp/detail')
 export class InquiryDetailController {
-  constructor(private readonly dt: InquiryDetailService) {}
+    constructor(private readonly dt: InquiryDetailService) {}
 
-  @Post('create')
-  create(@Body() createDto: createDetailDto) {
-    return this.dt.create(createDto);
-  }
+    @Post('create')
+    create(@Body() dto: createDetailDto) {
+        return this.dt.create(dto);
+    }
+
+    @Post('update')
+    update(@Body() dto: updateDetailDto) {
+        return this.dt.update(dto);
+    }
 }

0cf4785e70a1175bf7c43fef63b54d39bce54b62
Perapatr <perapatr@mitsubishielevatorasia.co.th>
2026-06-10 13:04:31 +0700

    feat: add PsCi module with controller, service, and repository; implement data form retrieval and logging functionality

--- src/webform/psform/psform.module.ts
+++ src/webform/psform/psform.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PsFileModule } from './ps-file/ps-file.module';
+import { PsCiModule } from './ps-ci/ps-ci.module';
 
 @Module({
-    imports: [PsFileModule],
+    imports: [PsFileModule, PsCiModule],
 })
 export class PsFormModule {}

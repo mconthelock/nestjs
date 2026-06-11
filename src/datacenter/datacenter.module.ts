a000dba9273898ef602486c4cccfc9bd8467d577
punnawichs <punnawichs@mitsubishielevatorasia.co.th>
2026-06-11 16:08:46 +0700

    API's PS-RP add module psform, datacenter

--- src/datacenter/datacenter.module.ts
+++ src/datacenter/datacenter.module.ts
@@ -6,8 +6,9 @@ import { F002kpModule } from './f002kp/f002kp.module';
 import { M001KpbmModule } from './m001-kpbm/m001-kpbm.module';
 import { S020kpModule } from './s020kp/s020kp.module';
 import { S049kpModule } from './s049kp/s049kp.module';
+import { J002mpModule } from './j002mp/j002mp.module';
 
 @Module({
-  imports: [F110kpModule, S011mpModule, F001kpModule, F002kpModule, M001KpbmModule, S020kpModule, S049kpModule],
+  imports: [F110kpModule, S011mpModule, F001kpModule, F002kpModule, M001KpbmModule, S020kpModule, S049kpModule, J002mpModule],
 })
 export class DatacenterModule {}

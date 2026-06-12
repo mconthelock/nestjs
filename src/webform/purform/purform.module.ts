d6e8b5de76f4002ffbcebae86ca0536a186bc74d
sutthipongjab <sutthipongjab@gmail.com>
2026-06-12 15:18:16 +0700

    fix: update PurFormModule imports to include PurCpmModule

--- src/webform/purform/purform.module.ts
+++ src/webform/purform/purform.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { PurFileModule } from './pur-file/pur-file.module';
 import { PurNvfModule } from './pur-nvf/purnvf.module';
 import { PurnvfFormModule } from './pur-nvf/purnvf_form/purnvf_form.module';
-import { PurnvfLocationModule } from './pur-nvf/purnvf_location/purnvf_location.module';
+import { PurCpmModule } from './pur-cpm/pur-cpm.module';
 
 @Module({
-    imports: [ PurFileModule, PurNvfModule, PurnvfFormModule , PurnvfFormModule ],
+    imports: [PurFileModule, PurNvfModule, PurnvfFormModule, PurCpmModule],
 })
 export class PurFormModule {}

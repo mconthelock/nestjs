740d652ac7a8fe332609e4a56af71433ba6e5e3f
Chalormsak S. <s.chalormsak@gmail.com>
2026-06-24 09:07:47 +0700

    Enhance email notification system: update Power BI email template, add inquiry email template, and modify sendMail method to support dynamic content

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -16,6 +16,9 @@ module.exports = {
             wait_ready: true,
             listen_timeout: 60000,
             kill_timeout: 5000,
+            env: {
+                NEST_DEBUG: 'true', // เปิดใช้งานตัว Debug Logger
+            },
         },
     ],
 };

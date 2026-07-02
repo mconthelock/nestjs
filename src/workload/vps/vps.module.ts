972d5140cecd95e12840b9b0911a8b85f692b173
Perapatr <perapatr@mitsubishielevatorasia.co.th>
2026-07-02 10:59:51 +0700

    feat: implement order detail retrieval and refactor get list order method

--- src/workload/vps/vps.module.ts
+++ src/workload/vps/vps.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { VpsService } from './vps.service';
 import { VpsController } from './vps.controller';
 import { VpsRepository } from './vps.repository';
+import { ConectionModule } from 'src/as400/conection/conection.module';
 
 
 @Module({
   controllers: [VpsController],
-
+  imports: [ConectionModule],
   providers: [VpsService, VpsRepository],
 })
 export class VpsModule {}

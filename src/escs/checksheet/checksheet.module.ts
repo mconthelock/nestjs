5781f15dc76e1be82c3200c48f35f5b84eace3e6
Pathanapong Sokpukeaw <patanapo@mitsubishielevatorasia.co.th>
2026-07-16 14:26:15 +0700

    update mfg-feeder

--- src/escs/checksheet/checksheet.module.ts
+++ src/escs/checksheet/checksheet.module.ts
@@ -1,15 +1,9 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { ChecksheetController } from './checksheet.controller';
 import { ChecksheetService } from './checksheet.service';
 import { ChecksheetRepository } from './checksheet.repository';
-import { GetOrderModule } from '../get-order/get-order.module';
 
 @Module({
-    imports: [
-        TypeOrmModule.forFeature([], 'escsConnection'),
-        GetOrderModule
-    ],
     controllers: [ChecksheetController],
     providers: [ChecksheetService, ChecksheetRepository],
 })
--- src/escs/mfg-feeder/dto/get-idtag.dto.ts
+++ src/escs/mfg-feeder/dto/get-idtag.dto.ts
@@ -1,6 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class GetIdTagDto {
+    @ApiProperty({
+        example: 'C604270001B',
+        description: 'Control Number'
+    })
     @IsString()
     @IsNotEmpty()
     controlNo: string;

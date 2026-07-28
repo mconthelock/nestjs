43ac42de5b503ace8729e66d5b103d7a4c62f2bf
onthelock <s.chalormsak@gmail.com>
2026-07-29 00:47:06 +0700

    fix: update database connection from 'webformConnection' to 'amecConnection' in Pposition module and service

--- src/amec/pposition/pposition.service.ts
+++ src/amec/pposition/pposition.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class PpositionService {
     constructor(
-        @InjectRepository(PPOSITION, 'webformConnection')
+        @InjectRepository(PPOSITION, 'amecConnection')
         private readonly spos: Repository<PPOSITION>,
     ) {}
 

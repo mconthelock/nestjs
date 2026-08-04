8af20b7615cef1ae5bc0fde0a4a9524457ce3813
Chalormsak S. <chalorms@MitsubishiElevatorAsia.co.th>
2026-08-04 15:31:08 +0700

    feat: add UNIFORM_RIGHT entity and implement rights retrieval in UniformService

--- src/gpreport/uniform/uniform.service.ts
+++ src/gpreport/uniform/uniform.service.ts
@@ -6,17 +6,42 @@ import { CreateUniformDto } from './dto/create-uniform.dto';
 import { UpdateUniformDto } from './dto/update-uniform.dto';
 
 import { UNIFORM } from '../../common/Entities/gpreport/table/UNIFORM.entity';
+import { UNIFORM_RIGHT } from '../../common/Entities/gpreport/table/UNIFORM_RIGHT.entity';
+import { UsersService } from '../../amec/users/users.service';
 
 @Injectable()
 export class UniformService {
     constructor(
         @InjectRepository(UNIFORM, 'gpreportConnection')
         private readonly uniform: Repository<UNIFORM>,
+
+        @InjectRepository(UNIFORM_RIGHT, 'gpreportConnection')
+        private readonly right: Repository<UNIFORM_RIGHT>,
+
+        private UsersService: UsersService,
     ) {}
 
     findAll() {
         return this.uniform.find({
             relations: ['category'],
         });
+    }
+
+    async findRights() {
+        const rights = await this.right.find();
+        const users = await this.UsersService.search();
+        const userFiltered = users.filter(
+            (u) => u.CSTATUS === '1' && parseInt(u.SPOSCODE) < 80,
+        );
+        const data = userFiltered.map((user) => {
+            const userRights = rights.filter(
+                (right) => right.EMPCOD === user.SEMPNO,
+            );
+            return {
+                ...user,
+                rights: userRights,
+            };
+        });
+        return data;
     }
 }

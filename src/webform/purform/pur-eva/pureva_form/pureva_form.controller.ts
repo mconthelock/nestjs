59ca4b9f9dac4be8be9828796323bb4180d3e9b5
ksatidpaisankul <ksatidpaisankul@gmail.com>
2026-08-07 17:11:28 +0700

    PUR-EVA

--- src/webform/purform/pur-eva/pureva_form/pureva_form.controller.ts
+++ src/webform/purform/pur-eva/pureva_form/pureva_form.controller.ts
@@ -28,6 +28,25 @@ export class PurevaFormController {
     return this.purevaFormService.findOne(+id);
   }
 
+  @Patch('update')
+  async update(         // ใช้สำหรับรับเงื่อนไขในการค้นหา (เช่น id หรือ key) ผ่าน Query Parameters
+    @Body() dto: UpdatePurevaFormDto   // ใช้สำหรับรับข้อมูลใหม่ที่ต้องการอัปเดตผ่าน Request Body
+  ) {
+    try {
+      const { NFRMNO , VORGNO , CYEAR , CYEAR2 , NRUNNO , ...data } = dto;
+      const form= {
+        NFRMNO : NFRMNO,
+        VORGNO : VORGNO,
+        CYEAR : CYEAR,
+        CYEAR2 : CYEAR2,
+        NRUNNO : NRUNNO 
+      }
+      return await this.purevaFormService.update(form, data);
+    } catch (error) {
+      throw new Error('Update PUR-EVA Form Error: ' + error.message);
+    }
+  }
+
   // @Patch(':id')
   // update(@Param('id') id: string, @Body() updatePurevaFormDto: UpdatePurevaFormDto) {
   //   return this.purevaFormService.update(+id, updatePurevaFormDto);

bbce8382cf27649452cb9bfc33a9ce08a1cdb47a
Supamid <supamid@MitsubishiElevatorAsia.co.th>
2026-08-24 15:20:58 +0700

    update expat 24/08/2026

--- src/gpreport/expat/expat.module.ts
+++ src/gpreport/expat/expat.module.ts
@@ -1,20 +1,14 @@
 import { Module } from '@nestjs/common';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ExpatController } from './expat.controller';
 import { ExpatService } from './expat.service';
 import { ExpatRepository } from './expat.repository';
+import { User } from 'src/amec/users/entities/user.entity';
 
 @Module({
-    controllers: [
-        ExpatController,
-    ],
-    providers: [
-        ExpatService,
-        ExpatRepository,
-    ],
-    exports: [
-        ExpatService,
-        ExpatRepository,
-    ],
+    imports: [TypeOrmModule.forFeature([User], 'gpreportConnection')],
+    controllers: [ExpatController],
+    providers: [ExpatService, ExpatRepository],
+    exports: [ExpatService, ExpatRepository],
 })
 export class ExpatModule {}

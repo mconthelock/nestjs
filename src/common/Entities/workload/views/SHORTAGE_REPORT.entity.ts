d661037444a3b5551063695866575a1a346e1ede
Pornprasit Kansunthia <pornpsit@mitsubishielevatorasia.co.th>
2026-08-25 16:10:56 +0700

    refactor: clean up code formatting and improve readability in shortage module and related files

--- src/common/Entities/workload/views/SHORTAGE_REPORT.entity.ts
+++ src/common/Entities/workload/views/SHORTAGE_REPORT.entity.ts
@@ -8,55 +8,55 @@ export class VW_SHORTAGE_REPORT {
   //หมายเลขผู้ซื้อ  
   @ViewColumn()
   BUYER: string;
-  
+
   @ViewColumn()
   JOBITEM: string;
-  
+
   @ViewColumn()
   ITEM: string;
-  
+
   @ViewColumn()
   DRAWING: string;
-  
+
   @ViewColumn()
   DESCRIPTION: string;
-  
+
   @ViewColumn()
   ONHAND: number;
-  
+
   @ViewColumn()
   ALLOCAT: number;
-  
+
   @ViewColumn({ name: 'TOTAL_QTY' })
   BALANCE: number;
-  
+
   @ViewColumn()
   QTY_N5: string;
-  
+
   @ViewColumn()
   QTY_N4: string;
-  
+
   @ViewColumn()
   QTY_N3: string;
-  
+
   @ViewColumn()
   QTY_N2: string;
-  
+
   @ViewColumn()
   QTY_N1: string;
-  
+
   @ViewColumn()
   QTY_N0: string;
-  
+
   @ViewColumn({ name: 'TOTAL_QTY' })
   TOTAL_SHORT: number;
-  
+
   @ViewColumn({ name: 'PVEND' })
   VENCODE: number;
-  
+
   @ViewColumn()
   VNDNAM: string;
-  
+
   //หมายเลข PO
   @ViewColumn()
   PONO: number;
@@ -71,16 +71,17 @@ export class VW_SHORTAGE_REPORT {
 
   //จำนวน REQUEST
   @ViewColumn()
-  PO_RQ : number;
+  PO_RQ: number;
 
   //จำนวน ALLOCATE (จำนวนที่รอรับ)
   @ViewColumn()
   REMAIN_PO: number;
-  
+
   //วันที่ต้องการรับสินค้า
   @ViewColumn()
   DUEDATE: number;
-  
+
+  //วันที่ ETD
   @ViewColumn()
   ETD: Date;
 
@@ -96,20 +97,22 @@ export class VW_SHORTAGE_REPORT {
   @ViewColumn()
   ARV_QTY: number;
 
+  //หมายเลข Invoice
   @ViewColumn()
   INV_NO: string;
+
   //COMMENT
   @ViewColumn()
   COMMENT_PUR: string;
-  
+
   //NEXT_REPLY
   @ViewColumn()
   NEXT_REPLY: Date;
-  
+
   //CAUSE_OF
   @ViewColumn()
   CAUSE_OF: string;
-  
+
   //REMARK
   @ViewColumn()
   REMARK: string;
@@ -117,7 +120,7 @@ export class VW_SHORTAGE_REPORT {
   //UPDATE DATE
   @ViewColumn()
   UPDATE_DATE: Date;
-  
+
   //USER UPDATE
   @ViewColumn()
   USER_UPDATE: string;
--- src/workload/shortage/shortage.repository.ts
+++ src/workload/shortage/shortage.repository.ts
@@ -7,12 +7,14 @@ import { BaseRepository } from "src/common/repositories/base-repository";
 import { DataSource } from "typeorm";
 import { CreatePurTrackingDto } from "./dto/create-purtracking.dto";
 import { UpdatePurTrackingDto } from "./dto/update-purtracking.dto";
+import { ConectionService } from "src/as400/conection/conection.service";
 
 //ติดต่อ กับฐานข้อมูล workload
 @Injectable()
 export class ShortageRepository extends BaseRepository {
     constructor(
         @InjectDataSource('workloadConnection') private ds: DataSource,
+        private as400: ConectionService,
     ) {
         super(ds);
     }
@@ -22,31 +24,35 @@ export class ShortageRepository extends BaseRepository {
      * @returns 
      */
     async get_headprod5() {
-        return this.getRepository(SHORTAGE_ISSUE).find({take:1});
+        return this.getRepository(SHORTAGE_ISSUE).find({ take: 1 });
     }
-    
+    /**
+     * get_shortagereport
+     * @description ดึงข้อมูล shortage report โดยใช้ entity VW_SHORTAGE_REPORT
+     * @returns 
+     */
     async get_shortagereport() {
         return this.getRepository(VW_SHORTAGE_REPORT).find({
-        order: {
-            //BUYER: 'ASC',      // จัดกลุ่ม Buyer ก่อน
-            //JOBITEM: 'ASC',    // ตามด้วย Job Item
-            ITEM: 'ASC',       // และ Item 
-        }
-    });  
-  }
-
-    async get_shortagereport_noentity(){
-        return this.getRepository(VW_SHORTAGE_REPORT)
-        .createQueryBuilder('report')
-        .getMany();   
+            order: {
+                //BUYER: 'ASC',      // จัดกลุ่ม Buyer ก่อน
+                //JOBITEM: 'ASC',    // ตามด้วย Job Item
+                ITEM: 'ASC',       // และ Item 
+            }
+        });
     }
-    
+
+    async get_shortagereport_noentity() {
+        return this.getRepository(VW_SHORTAGE_REPORT)
+            .createQueryBuilder('report')
+            .getMany();
+    }
+
     /**
      * get_shortagereport_sql
      * @description ดึงข้อมูล shortage report โดยใช้ sql query
      * @returns 
      */
-    async get_shortagereport_sql(){
+    async get_shortagereport_sql() {
         return this.manager.query(`SELECT * FROM WORKLOAD.VW_SHORTAGE_REPORT`);
     }
 
@@ -68,14 +74,24 @@ export class ShortageRepository extends BaseRepository {
      * @returns 
      */
     async update_short_pur_tracking(
-    {PONO,PPROD,PLINE,PORD}: {
-    PONO: number;
-    PPROD: string;
-    PLINE: number;
-    PORD: number;
-   },
-   data: UpdatePurTrackingDto,) {
-        return this.getRepository(SHORT_PUR_TRACKING).update({PONO, PPROD, PLINE, PORD}, data);
-   }
+        { PONO, PPROD, PLINE, PORD }: {
+            PONO: number;
+            PPROD: string;
+            PLINE: number;
+            PORD: number;
+        },
+        data: UpdatePurTrackingDto,) {
+        return this.getRepository(SHORT_PUR_TRACKING).update({ PONO, PPROD, PLINE, PORD }, data);
+    }
+
+    async get_poinvoice(): Promise<any[]> {
+        //const sql = `SELECT * FROM RTNLIBF.Q46054OL WHERE Q46O01 = '${order}' AND Q46O02 = '${packing}'`;
+        const sql = `SELECT PONO, RTRIM(POINV) AS POINV, POLINE, RTRIM(POITM) AS POITM, POQTY,  PORQTY, PODTE, POETD,POTME, POETA, RTRIM(POSHIP) AS POSHIP, POETAA, POKQTY, RTRIM(POFLAG) AS POFLAG
+        FROM RTNLIBF.PUR700O 
+        WHERE PODTE >= (YEAR(CURRENT DATE - 2 YEARS) * 10000) + (MONTH(CURRENT DATE - 2 YEARS) * 100) + DAY(CURRENT DATE - 2  YEARS)  AND  POETD >0  AND  POFLAG='Y'`;
+
+        // หาก query ชุดนี้ต้องการดึงจาก AS400 Connection สามารถเปลี่ยนจาก this.wk เป็น DataSource ของ AS400 ที่ Inject เอาไว้ได้เลย
+        return await this.as400.runQuery(sql);
+    }
 
 }

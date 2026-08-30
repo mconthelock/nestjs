2b58d2ad3272d0bc8cbb5a727015fcc2b6ac77f3
onthelock <s.chalormsak@gmail.com>
2026-08-31 06:31:06 +0700

    feat: implement create, update, and delete functionality for flow master; add corresponding DTOs and service methods

--- src/webform/flowmst/flowmst.controller.ts
+++ src/webform/flowmst/flowmst.controller.ts
@@ -11,6 +11,8 @@ import {
 import { Request } from 'express';
 import { FlowmstService } from './flowmst.service';
 import { ApiTags, ApiOperation, ApiParam } from '@nestjs/swagger';
+import { CreateFlowmstDto } from './dto/create-flowmst.dto';
+import { UpdateFlowmstDto } from './dto/update-flowmst.dto';
 
 @ApiTags('Flow master')
 @Controller('flowmst')
@@ -50,5 +52,39 @@ export class FlowmstController {
         @Param('CYEAR') CYEAR: string,
     ) {
         return this.flowmstService.findFlowMaster(NFRMNO, VORGNO, CYEAR);
+    }
+
+    @Post('create')
+    @ApiOperation({
+        summary: 'Create a new flow master',
+    })
+    createFlowMaster(@Body() body: CreateFlowmstDto) {
+        return this.flowmstService.createFlowMaster(body);
+    }
+
+    @Patch('update')
+    @ApiOperation({
+        summary: 'Update an existing flow master',
+    })
+    @ApiParam({ name: 'NFRMNO', example: 13, required: true })
+    @ApiParam({ name: 'VORGNO', example: '000101', required: true })
+    @ApiParam({ name: 'CYEAR', example: '25', required: true })
+    updateFlowMaster(@Body() body: UpdateFlowmstDto) {
+        return this.flowmstService.updateFlowMaster(body);
+    }
+
+    @Delete('delete/:NFRMNO/:VORGNO/:CYEAR')
+    @ApiOperation({
+        summary: 'Delete an existing flow master',
+    })
+    @ApiParam({ name: 'NFRMNO', example: 13, required: true })
+    @ApiParam({ name: 'VORGNO', example: '000101', required: true })
+    @ApiParam({ name: 'CYEAR', example: '25', required: true })
+    deleteFlowMaster(
+        @Param('NFRMNO') NFRMNO: number,
+        @Param('VORGNO') VORGNO: string,
+        @Param('CYEAR') CYEAR: string,
+    ) {
+        return this.flowmstService.deleteFlowMaster(NFRMNO, VORGNO, CYEAR);
     }
 }

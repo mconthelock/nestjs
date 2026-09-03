65f2920f840183724a1cac14332673919133d1c8
Supamid <supamid@MitsubishiElevatorAsia.co.th>
2026-09-03 16:32:40 +0700

    update jig 03/09/2026

--- src/iedoc/iedoc.module.ts
+++ src/iedoc/iedoc.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { JigModule } from './jig/jig.module';
 
-
 @Module({
     imports: [
-    JigModule],
+        JigModule,
+    ],
 })
-export class gpreportModule {}
+export class IedocModule {}

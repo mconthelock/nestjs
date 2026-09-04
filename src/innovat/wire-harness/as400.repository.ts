8b7901177071f80a117e19f5dc38a5ebc406faa0
Pathanapong Sokpukeaw <patanapo@mitsubishielevatorasia.co.th>
2026-09-04 15:15:57 +0700

    update wire-harness autoplan

---
+++ src/innovat/wire-harness/as400.repository.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@nestjs/common';
+import { ConectionService } from 'src/as400/conection/conection.service';
+
+@Injectable()
+export class AS400Repository {
+    constructor(
+        private readonly as400: ConectionService,
+    ) {}
+
+    getProductionPlan() {
+        return this.as400.runQuery(`
+            SELECT
+                F1.F01R01 AS CTRLNO,
+                F2.F02R03 AS PROCESS,
+                S.M8K01 AS PROD,
+                S.M8K02 AS P,
+                S.M8K03 AS MFGNO,
+                F1.F01R08 AS PROJ,
+                Q9.Q9TYP AS MODEL,
+                F1.F01R04 AS DWG,
+                F1.F01R10 AS QTY,
+                CB.Q41K14 AS MATERIAL,
+                CB.Q41K20 AS ITEMCODE,
+                DECIMAL(CB.Q41K16, 8, 0) / 1000 AS CUT,
+                Q3.Q43K06 AS REMARK,
+                ROW_NUMBER() OVER (
+                    PARTITION BY F1.F01R01, F2.F02R03
+                    ORDER BY CB.Q41K05
+                ) AS RN
+            FROM RTNLIBF.M008KP S
+            JOIN RTNLIBF.Q90010P2 Q9
+                ON S.M8K03 = Q9.Q9ORD
+            JOIN SHOPF.F001KP F1
+                ON S.M8K03 = F1.F01R07
+            JOIN SHOPF.F002KP F2
+                ON F1.F01R01 = F2.F02R01
+            JOIN RTNLIBF.Q141KP Q1
+                ON F1.F01R07 = Q1.Q41K01
+                AND F1.F01R04 = Q1.Q41K08
+            LEFT JOIN RTNLIBF.Q141KP CB
+                ON Q1.Q41K01 = CB.Q41K01
+                AND Q1.Q41K02 = CB.Q41K02
+                AND Q1.Q41K03 = CB.Q41K03
+                AND Q1.Q41K04 = CB.Q41K04
+                AND CB.Q41K10 LIKE 'CABLE%'
+                AND SUBSTR(Q1.Q41K08, 1, LOCATE(' ', Q1.Q41K08 || ' ') - 1)
+                    = SUBSTR(CB.Q41K08, 1, LOCATE(' ', CB.Q41K08 || ' ') - 1)
+            LEFT JOIN RTNLIBF.Q143KP Q3
+                ON Q1.Q41K01 = Q3.Q43K01
+                AND Q1.Q41K02 = Q3.Q43K02
+                AND Q1.Q41K03 = Q3.Q43K03
+                AND Q1.Q41K04 = Q3.Q43K04
+                AND Q1.Q41K05 = Q3.Q43K05
+            WHERE F2.F02R03 IN ('B4CC06')
+        `);
+    }
+}
--- src/innovat/wire-harness/wire-harness.repository.ts
+++ src/innovat/wire-harness/wire-harness.repository.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectDataSource } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
 import { BaseRepository } from 'src/common/repositories/base-repository';
+import { WHN_PRODUCTION_PLAN } from 'src/common/Entities/innovat/table/WHN_PRODUCTION_PLAN.entity';
 
 @Injectable()
 export class WireHarnessRepository extends BaseRepository {
@@ -12,66 +13,15 @@ export class WireHarnessRepository extends BaseRepository {
         super(ds);
     }
 
-    async deleteProductionPlan() {
-        return this.ds.query(`
-            DELETE FROM WHN_PRODUCTION_PLAN
-        `);
+    saveProductionPlan(data: Partial<WHN_PRODUCTION_PLAN>[]) {
+        return this.getRepository(WHN_PRODUCTION_PLAN).save(data);
     }
 
-    async insertProductionPlan(data: any[]) {
-        for (const row of data) {
-            await this.ds.query(
-                `
-                INSERT INTO WHN_PRODUCTION_PLAN (
-                    CTRLNO,
-                    PROCESS,
-                    PROD,
-                    P,
-                    MFGNO,
-                    PROJ,
-                    MODEL,
-                    DWG,
-                    QTY,
-                    MATERIAL,
-                    ITEMCODE,
-                    CUT,
-                    REMARK
-                )
-                VALUES (
-                    :1,:2,:3,:4,:5,:6,:7,:8,:9,:10,:11,:12,:13
-                )
-                `,
-                [
-                    row.CTRLNO,
-                    row.PROCESS,
-                    row.PROD,
-                    row.P,
-                    row.MFGNO,
-                    row.PROJ,
-                    row.MODEL,
-                    row.DWG,
-                    row.QTY,
-                    row.MATERIAL,
-                    row.ITEMCODE,
-                    row.CUT,
-                    row.REMARK,
-                ],
-            );
-        }
+    getProductionPlan(proc: string) {
+        return this.getRepository(WHN_PRODUCTION_PLAN).find({
+            where: {
+                PROCESS: proc
+            }
+        });
     }
-
-    async getProductionPlan() {
-        return this.ds.query(`
-            SELECT *
-            FROM VW_AUTOPLAN
-        `);
-    }
-
-    // findByName(name: string) {
-    //     return this.getRepository(MACHINE_NAME).findOne({
-    //         where: {
-    //             MC_NAME: name
-    //         },
-    //     });
-    // }
 }

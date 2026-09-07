c853f1b99528bb69ec7935ad959838bad86e36af
sutthipongjab <sutthipongjab@gmail.com>
2026-09-07 13:06:19 +0700

    refactor: uncomment S001kpModule and S001kpService imports in packing list module and service

--- src/workload/dpms_packing_list_main/dpms_packing_list_main.module.ts
+++ src/workload/dpms_packing_list_main/dpms_packing_list_main.module.ts
@@ -4,7 +4,7 @@ import { DpmsPackingListMainController } from './dpms_packing_list_main.controll
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DPMS_PACKING_LIST_MAIN } from 'src/common/Entities/workload/views/DPMS_PACKING_LIST_MAIN.entity';
 import { DpmsPackingListMainRepository } from './dpms_packing_list_main.repository';
-// import { S001kpModule } from 'src/as400/rtnlibf/s001kp/s001kp.module';
+import { S001kpModule } from 'src/as400/rtnlibf/s001kp/s001kp.module';
 import { S011mpModule } from 'src/datacenter/s011mp/s011mp.module';
 
 @Module({
@@ -13,7 +13,7 @@ import { S011mpModule } from 'src/datacenter/s011mp/s011mp.module';
             [DPMS_PACKING_LIST_MAIN],
             'workloadConnection',
         ),
-        // S001kpModule,
+        S001kpModule,
         S011mpModule,
     ],
     controllers: [DpmsPackingListMainController],

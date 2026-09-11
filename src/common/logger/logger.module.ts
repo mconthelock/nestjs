84e79bbfc9e59832d2bca17fc63013c59ca97585
Chalormsak S. <chalorms@MitsubishiElevatorAsia.co.th>
2026-09-11 09:03:34 +0700

    feat: implement performance logging; add PerformanceLoggerProvider and integrate with LoggerController and DoactionFlowService

--- src/common/logger/logger.module.ts
+++ src/common/logger/logger.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PerformanceLoggerProvider } from './performance-logger.provider';
 import { LoggerController } from './logger.controller';
 import { LoggerService } from './logger.service';
 
 @Module({
-  controllers: [LoggerController],
-  providers: [LoggerService],
+    controllers: [LoggerController],
+    providers: [LoggerService, PerformanceLoggerProvider],
 })
 export class LoggerModule {}

a33de878289c855322c91aba19beee59f7074b5d
Perapatr <perapatr@mitsubishielevatorasia.co.th>
2026-09-16 08:54:15 +0700

    refactor: update getTRNBarcode method to use detailed SQL query with joins for enhanced data retrieval

--- src/workload/block_packing/block_packing.repository.ts
+++ src/workload/block_packing/block_packing.repository.ts
@@ -17,8 +17,24 @@ export class BlockPackingRepository extends BaseRepository {
     }
 
     async getTRNBarcode() {
-        return this.manager.query(
-            `SELECT * FROM TRNBarcode where Production > '2026000'`,
+        return this.datacenterDs.query(
+            `SELECT
+                t.*,
+                p.VPS03,
+                p.VPS04,
+                ct.VPS06 AS CASETYPE
+            FROM TRNBARCODE t
+            JOIN PIDVPS p
+                ON t.ORDERNO = p.VPS01
+                AND t.BLOCK = p.VPS02
+                AND t.CASENO = p.VPS06
+                AND p.VPS03 = 'CASENO'
+            JOIN PIDVPS ct
+                ON ct.VPS01 = p.VPS01
+                AND ct.VPS02 = p.VPS02
+                AND ct.VPS03 = 'CASETYPE'
+                AND ct.VPS04 = p.VPS04
+            WHERE PRODUCTION > '2026000'`,
         );
     }
 
